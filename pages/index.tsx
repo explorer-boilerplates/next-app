afbf3cf337e297c33752975472b42089e8f88719
user <progexplorer@gmail.com>
2020-05-08 16:48:15 +0200

    refactor: add tailwind classes to element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
 import { NextPage } from "next";
+import { css } from "@emotion/core";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import tw from "twin.macro";
 import { Basic, Combined, Animated, bounce } from "../shared/styles";
 
 const Home: NextPage = () => (
@@ -8,6 +11,14 @@ const Home: NextPage = () => (
       With <code>:hover</code>.
     </Combined>
     <Animated animation={bounce}>Let&apos;s bounce.</Animated>
+    <div
+      css={css`
+        ${tw`w-10 h-10 text-teal-600 bg-gray-200`}
+      `}
+    >
+      {" "}
+      Hello
+    </div>
   </div>
 );
 
