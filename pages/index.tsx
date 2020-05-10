fd2f39f516dbde25d87a468b02f671a95a410862
user <progexplorer@gmail.com>
2020-05-10 15:49:57 +0200

    fix: a11y issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import { Basic, Combined, Animated, bounce } from "../shared/styles";
 import HelloWorld from "../components/hello";
 
 const Home: NextPage = () => (
-  <div>
+  <main>
+    <h1>This is the heading</h1>
     <Basic>Cool Styles</Basic>
     <Combined>
       With <code>:hover</code>.
     </Combined>
     <Animated animation={bounce}>Let&apos;s bounce.</Animated>
     <HelloWorld />
-  </div>
+  </main>
 );
 
 export default Home;
