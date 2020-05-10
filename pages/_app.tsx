41287348a61834b55c4bc65699b5a1ddfecefe82
user <progexplorer@gmail.com>
2020-05-10 15:48:30 +0200

    refactor: simplify emotion config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,5 @@
-import * as React from "react";
+import React from "react";
 import NextApp from "next/app";
-import { CacheProvider } from "@emotion/core";
-
-// Use only { cache } from 'emotion'. Don't use { css }.
-// eslint-disable-next-line emotion/no-vanilla
-import { cache } from "emotion";
 
 import { globalStyles } from "../shared/styles";
 
@@ -12,10 +7,10 @@ export default class App extends NextApp {
   render(): JSX.Element {
     const { Component, pageProps } = this.props;
     return (
-      <CacheProvider value={cache}>
+      <>
         {globalStyles}
         <Component {...pageProps} />
-      </CacheProvider>
+      </>
     );
   }
 }
