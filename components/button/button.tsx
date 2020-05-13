afb1ef7da4cc9d1123ea63fe8dcf06012550535c
user <progexplorer@gmail.com>
2020-05-13 09:55:23 +0200

    refactor: add css prop to button

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,4 +1,7 @@
 import React, { FC } from "react";
+import { SerializedStyles, css } from "@emotion/core";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import tw from "twin.macro";
 
 export type ButtonProps = {
   /**
@@ -10,17 +13,32 @@ export type ButtonProps = {
    * Simple click handler
    */
   readonly onClick?: () => void;
+  /**
+   * Styles
+   */
+  css?: SerializedStyles;
 };
 
 /**
  * The world's most _basic_ button
  */
-export const Button: FC<ButtonProps> = ({ label, onClick }: ButtonProps) => {
+export const Button: FC<ButtonProps> = ({
+  label,
+  onClick,
+  ...props
+}: ButtonProps) => {
   // eslint-disable-next-line no-console
   console.log("Error");
 
   return (
-    <button onClick={onClick} type="button">
+    <button
+      onClick={onClick}
+      type="button"
+      css={css`
+        ${tw`text-indigo-300`}
+      `}
+      {...props}
+    >
       {label}
     </button>
   );
