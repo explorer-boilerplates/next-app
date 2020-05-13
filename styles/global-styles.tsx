8c1a149cd4ab0f23ed3e00a1f09b7c5e1fd1e6d3
user <progexplorer@gmail.com>
2020-05-13 09:36:02 +0200

    feat: create global styles component

---
+++ styles/global-styles.tsx
@@ -0,0 +1,23 @@
+import { Global, css } from "@emotion/core";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import tw from "twin.macro";
+import tailwindReset from "./tailwind-reset";
+
+const globalStyles = (
+  <Global
+    styles={css`
+      ${tailwindReset}
+
+      html {
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+      }
+
+      body {
+        ${tw`font-sans leading-normal text-gray-900 bg-gray-100`}
+      }
+    `}
+  />
+);
+
+export default globalStyles;
