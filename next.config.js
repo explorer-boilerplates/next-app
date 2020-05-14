09792dc49cf18d0cd783e94a1444908d2f055f44
user <progexplorer@gmail.com>
2020-05-14 09:47:54 +0200

    build: remove experimental feature

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  experimental: {
-    jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
