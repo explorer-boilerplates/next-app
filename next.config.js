e255f2c05b5e75ea866249407d241a68af7795bf
user <progexplorer@gmail.com>
2020-05-14 09:30:02 +0200

    build: add svg loader

--- next.config.js
+++ next.config.js
@@ -2,4 +2,15 @@ module.exports = {
   experimental: {
     jsconfigPaths: true, // enables it for both jsconfig.json and tsconfig.json
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
